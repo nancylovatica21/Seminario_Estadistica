2d17df23a97d7cd9ba3ac1822b6a8a78d277c9f9
Fernando Alvarado <fernandoalvarado@ciencias.unam.mx>
2025-03-17 23:32:53 -0600

    Avance eje 2

--- Proyecto_1/Ejercicio_2/pruebas.r
+++ Proyecto_1/Ejercicio_2/pruebas.r
@@ -1,19 +1,39 @@
-library(purrr)
-
-numeros <- list(4, 9, 16, 25)
-a <- map(numeros, sqrt)
-
-
-print(a)
-
-print("--------------------")
-
-print(a[[1]])
+# Descripción: Este script contiene el código de la función de selección de modelos en base a su AIC y BIC|
+# Autor: Fernando Alvarado Palacios
+# Fecha: 2020-12-15
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------
+#Algoritmo de selección de modelos en base a su AIC y BIC
+#------------------------------------------------------------------------------------------------------------------------------------------
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------
+#Algoritmo malla (Aqui hare la malla para poder elevar a las variables a la potencia que se requiera)
 
 
+malla <- function(n, liminf, limsup, finura, des_Ponderada = 0 , limInfpon = 0, limSuppon = 2){
+  #Parametros
+  #n: número de parametros que tiene nuestro modelo 
+  #liminf: límite inferior de la malla, para las variables x
+  #limsup: límite superior de la malla, para las variables x
+  #finura: finura de la malla que deseamos
+  #des_Ponderada: si se requiere usar regresion ponderada
+  #limInfpon: límite inferior de la regresion ponderada
+  #limSuppon: límite superior de la regresion ponderada  
+  
+  # Crear un data frame con el número correcto de filas antes de agregar columnas
+  seq_values <- seq(liminf, limsup, by = finura)
+  df <- data.frame(matrix(ncol = 0, nrow = length(seq_values))) # Se crea con las filas correctas
 
+  for (i in 1:n) {
+    col_name <- paste("var_", i, sep = "")  # Crear nombre de columna
+    df[[col_name]] <- seq_values  # Asignar los valores de la secuencia
+  }
+  
+  if(des_Ponderada != 0){
+    df[["Ponderada"]] <- seq(limInfpon, limSuppon, length.out = length(seq_values))
+  }
 
+  return(expand.grid(df))
+}
